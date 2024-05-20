32d9e3a4d4b849f02880c82ed106108ef0641d53
GurudeepRahul <kgurudeeprahul@gmail.com>
2024-05-20 14:05:32 +0530

    Added the code for the Project

---
+++ routes/route.js
@@ -0,0 +1,18 @@
+const users = require("../controllers/user-controller")
+const multer  = require('multer')
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads")
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage })
+
+module.exports = (router) => {
+    router.post("/admin/add-users", upload.single('file'), users.addUsers);
+    router.post("/admin/send-email", users.sendEmail);
+    router.post("/admin/unsubscribe-user", users.unsubscribeUser);
+}
